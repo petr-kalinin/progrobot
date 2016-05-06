d32dcb8c536dd02fa38f8ef8870f06aa3c4e501c
Petr Kalinin <petr@kalinin.nnov.ru>
2016-05-06 22:17:45 +0300

    /so, /start and /help commands

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ client = pymongo.MongoClient()
 db = client.requests
 requests = db.requests
 
+HELP_MESSAGE = """
+The bot allows you to search across C++ and Python3 documentation, and across StackOverflow.
+
+To search for documentation, just type the entity you want to search for.
+
+For example:
+
+std vector insert — show documentation for C++'s <code>std::vector::insert()</code> method
+
+re — show documentation for Python3's <code>re</code> module
+
+re match — for this specific function from <code>re</code> module
+
+You can omit some of first tokens, for example, "vector insert" also works. You can separate tokens with any non-word characters you like, for example, "vector::insert" also works.
+
+You can also search StackOverflow by starting your request with /so command. For example, "/so javascript print to console".
+"""
 
 class YourBot(telepot.Bot):
     def __init__(self, *args, **kwargs):
@@ -33,9 +50,24 @@ class YourBot(telepot.Bot):
         if content_type != 'text':
             return
         try:
-            answer = reference.search(msg["text"])
-            if not answer:
-                answer = stackoverflow.search(msg["text"])
+            query = msg["text"]
+            if query == "/start" or query == "/help":
+                answer = HELP_MESSAGE
+            elif query == "/so":  # TODO: accept also /so@Progrobot
+                answer = "Please follow /so command by actual request to StackOverflow"
+            elif query[0:4] == "/so ":
+                answer = stackoverflow.search(query)
+            else:
+                answer = reference.search(query)
+                if not answer:
+                    answer = ("No reference found for your request. " +
+                              "Make sure you spelled all tokens in your request propery. " +
+                              "If you were asking about a specific method or function of some " +
+                              "module or class, you can make your request more generic by directly asking " +
+                              "about that module or class.\n\n" +
+                              "If you were intending to search StackOverflow for a question, " +
+                              "then prefix your request with /so command: \n" +
+                              "/so " + query)
             answer = re.sub(r'\n\n+', '\n\n', answer)
             print(answer)
             self.sendMessage(chat_id, answer, parse_mode="HTML")
