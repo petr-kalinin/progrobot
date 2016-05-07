a339dec75a037b18ccd247b569d15ddc33895ed4
Petr Kalinin <petr@kalinin.nnov.ru>
2016-05-07 22:28:40 +0300

    Better html2tele

--- html2tele.py
+++ html2tele.py
@@ -1,10 +1,10 @@
-# adapted from http://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 import html
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 import re
 
 SUPPORTED_TAGS = ('b', 'strong', 'i', 'em', 'code', 'pre')
+CODE_TAGS = ('code', 'pre')
 
 def find_href(attrs):
     for attr in attrs:
@@ -16,61 +16,81 @@ def find_href(attrs):
 class _HTMLToText(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self._buf = []
+        self.buf = []
+        self.last_text = []
         self.hide_output = False
-        self._tag_count = 0
+        self.tag_count = 0
+        self.current_tag = None
+        
+    def process_last_text(self):
+        text = "".join(self.last_text).strip()
+        if not self.current_tag in CODE_TAGS:
+            text = re.sub("\s+", " ", text)
+        self.buf.append(text)
+        self.last_text = []
+        
+    def push_tag(self, tag_str):
+        self.process_last_text()
+        self.buf.append(tag_str)
+        
+    def push_text(self, text):
+        self.last_text.append(text)
+        
+    def push_newlines(self, count = 1):
+        self.process_last_text()
+        self.buf.append("\n" * count)
 
     def handle_starttag(self, tag, attrs):
-        self._tag_count += 1
+        self.tag_count += 1
         if self.hide_output:
             return
-        elif tag in SUPPORTED_TAGS and (self._tag_count == 1):
-            self._buf.append("<"+tag+">")
-        elif tag == "a" and (self._tag_count == 1):
+        elif tag in SUPPORTED_TAGS and (self.tag_count == 1):
+            self.push_tag("<"+tag+">")
+            self.current_tag = tag
+        elif tag == "a" and (self.tag_count == 1):
             href = find_href(attrs)
-            self._buf.append("<a href='{0}'>".format(html.escape(href, quote=True)))
+            self.push_tag("<a href='{0}'>".format(html.escape(href, quote=True)))
+            self.current_tag = "a"
         elif tag in ('p', 'br', 'div', 'table'):
-            self._buf.append('\n')
+            self.push_newlines(2)
         elif tag in ('script', 'style'):
             self.hide_output = True
 
     def handle_startendtag(self, tag, attrs):
         if tag == 'br':
-            self._buf.append('\n')
+            self.push_newlines(2)
 
     def handle_endtag(self, tag):
         if tag in ('p', 'div', 'table'):
-            self._buf.append('\n')
-        if (tag in SUPPORTED_TAGS or tag == "a") and (self._tag_count == 1):
-            self._buf.append("</" + tag + ">")
+            self.push_newlines(2)
+        elif (tag in SUPPORTED_TAGS or tag == "a") and (self.tag_count == 1):
+            self.push_tag("</" + tag + ">")
+            self.current_tag = None
         elif tag in ('script', 'style'):
             self.hide_output = False
-        self._tag_count -= 1
+        self.tag_count -= 1
 
     def handle_data(self, text):
-        if text and not self.hide_output:
-            self._buf.append(html.escape(re.sub(r'\s+', ' ', text)))
+        print("data", text, self.hide_output)
+        if not self.hide_output:
+            self.push_text(html.escape(text))
 
     def handle_entityref(self, name):
         if name in name2codepoint and not self.hide_output:
-            #c = chr(name2codepoint[name])
-            self._buf.append("&" + name + ";")
+            code = name2codepoint[name]
+            self.push_text("&#" + str(code) + ";")
 
     def handle_charref(self, name):
         if not self.hide_output:
-            #n = int(name[1:], 16) if name.startswith('x') else int(name)
-            #self._buf.append(chr(n))
-            self._buf.append("&" + name + ";")
+            self.push_text("&" + name + ";")
 
     def get_text(self):
-        res = ''.join(self._buf)
-        res = re.sub(r' +', ' ', res)
-        res = re.sub(r'\n\n+', '\n\n', res)
-        res = re.sub(r' +\n', '\n', res)
-        res = re.sub(r'\n +', '\n', res)
+        self.push_tag("")
+        res = ''.join(self.buf)
         return res
 
 def html2tele(html):
+    print("html:", html)
     parser = _HTMLToText()
     parser.feed(html)
     parser.close()
