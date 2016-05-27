f42683e2454071e55ea0331f5d113fb72d6747f2
Petr Kalinin <petr@kalinin.nnov.ru>
2016-05-27 09:40:37 +0300

    Restored processing of \n\n\n (#20)

--- Handler.py
+++ Handler.py
@@ -1,5 +1,8 @@
+import re
+
 class Handler:
     def format_answer(self, text, **kwargs):
+        text = re.sub(r"\n(\s*\n)+", "\n\n", text)
         answer = kwargs
         answer["text"] = text
         return answer
